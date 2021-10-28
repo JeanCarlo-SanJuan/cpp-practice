9ac409b72a8c464994f5aacfea082982b314583a
JCSJ <sanjuan.jeancarlo@gmail.com>
2021-10-28 18:28:51 +0800

    added correct month limits. Todo: Leap year

--- zodiac_identifier.cpp
+++ zodiac_identifier.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 vector<vector<string>> zodiac_data = {
-    {"January", "19", "Capricorn", "Aquarius"},
-    {"Februrary", "18", "Aquarius", "Pisces"},
-    {"March", "18", "Pisces", "Aries"},
-    {"April", "18", "Aries", "Taurus"},
-    {"May", "18", "Taurus", "Gemini"},
-    {"June", "18", "Gemini", "Cancer"},
-    {"July", "22", "Cancer", "Leo"},
-    {"August", "22", "Leo", "Virgo"},
-    {"September", "22", "Virgo", "Libra"},
-    {"October", "22", "Libra", "Scorpio"},
-    {"November", "21", "Scorpio", "Sagittarius"},
-    {"December", "21", "Sagittarius", "Capricorn"}
+    {"January", "19", "31", "Capricorn", "Aquarius"},
+    {"Februrary", "18", "28", "Aquarius", "Pisces"},
+    {"March", "18", "31", "Pisces", "Aries"},
+    {"April", "18", "30" "Aries", "Taurus"},
+    {"May", "18", "31", "Taurus", "Gemini"},
+    {"June", "18", "30", "Gemini", "Cancer"},
+    {"July", "22", "31", "Cancer", "Leo"},
+    {"August", "22", "31", "Leo", "Virgo"},
+    {"September", "22","30", "Virgo", "Libra"},
+    {"October", "22", "31", "Libra", "Scorpio"},
+    {"November", "21", "30", "Scorpio", "Sagittarius"},
+    {"December", "21", "31", "Sagittarius", "Capricorn"}
 };
 
 void show_zodiac(int m, int day) {
-    if (day < 1 || day > 31) {
+    if (day < 1 || stoi(zodiac_data[m][2]) > 31) {
         cout << "Please type a valid day (1-31)\n";
     } else {
-        cout << "Zodiac sign for " << zodiac_data[m][0] << " " << day << " is " << ((day <= stoi(zodiac_data[m][1])) ? zodiac_data[m][2]:zodiac_data[m][3]) << endl;
+        cout << "Zodiac sign for " << zodiac_data[m][0] << " " << day << " is " << ((day <= stoi(zodiac_data[m][1])) ? zodiac_data[m][3]:zodiac_data[m][4]) << endl;
     }
 }
 
